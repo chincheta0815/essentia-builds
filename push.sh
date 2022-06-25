688242c6be393e42c05cbf3d6d301d52dd64f3fb
chincheta0815 <chincheta0815@users.noreply.github.com>
2022-06-25 10:48:56 +0200

    add aarch64

--- push.sh
+++ push.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 docker push mtgupf/essentia-builds:manylinux2014_x86_64
 docker push mtgupf/essentia-builds:manylinux2014_i686
+docker push chincheta0815/essentia-builds:manylinux2014_aarch64
